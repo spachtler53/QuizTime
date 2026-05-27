f3280a11df11a8251baa76e4e7d0a523d5167c02
N.Bakowsky <spachtler53@gmail.com>
2026-05-27 11:17:38 +0200

    Add try/except for invalid input

--- main.py
+++ main.py
@@ -1,30 +1,49 @@
-from pathlib import Path
+from questions import questions
+
 placeholder = 50 * "\033[96m_\033[94m"
-questions_file = Path("questions.py")
+
+
 def show_menu() -> None:
     print(placeholder)
-    print("\t\t\t"
-          "Willkommen bei QuizTimes!")
-    print(placeholder)
-    print("\033[3m\033[91mBitte Wähle im Menü aus:\n\033[0m")
-    print("\033[93m> 1. Schnelles Quiz starten\033[90m\n"
-          "\033[93m> 2. Themenbereich auswählen\033[90m\n"
-          "\033[93m> 3. Statistiken ansehen\033[90m")
+    print("\t\t\tWillkommen bei QuizTimes!")
     print(placeholder)
 
+    print("\033[3m\033[91mBitte wähle im Menü aus:\n\033[0m")
 
-def quiz_starten(questions: questions_file):
-      score = 0
+    print(
+        "\033[93m> 1. Schnelles Quiz starten\033[0m\n"
+        "\033[93m> 2. Themenbereich auswählen\033[0m\n"
+        "\033[93m> 3. Statistiken ansehen\033[0m"
+    )
 
-      for i in range (len(questions)):
-          print(f"\nFrage {i + 1}: {questions[i]['question']}")
-          for idx, option in enumerate(questions[i]['options']):
-              print(f"{idx + 1}. {option}")
-          answer = input("Deine Antwort (1-4): ")
-          if answer.isdigit() and int(answer) == questions[i]['answer']:
-              print("\033[92mRichtig!\033[0m")
-              score += 1
-          else:
-              print("\033[91mFalsch!\033[0m")
+    print(placeholder)
 
-quiz_starten(questions_file)
+
+def quiz_starten(questions):
+    score = 0
+
+    for i, q in enumerate(questions):
+        print(f"\nFrage {i + 1}: {q['question']}")
+
+        for idx, option in enumerate(q["options"]):
+            print(f"{idx + 1}. {option}")
+
+        answer = input("Deine Antwort (1-4): ")
+
+        if answer.isdigit():
+            answer_index = int(answer) - 1
+
+            if answer_index == q["correct_index"]:
+                print("\033[92mRichtig!\033[0m")
+                score += 1
+            else:
+                print("\033[91mFalsch!\033[0m")
+                print(f"Erklärung: {q['explanation']}")
+        else:
+            print("Bitte nur Zahlen eingeben.")
+
+    print(f"\nDu hast {score}/{len(questions)} richtig.")
+
+
+show_menu()
+quiz_starten(questions)
